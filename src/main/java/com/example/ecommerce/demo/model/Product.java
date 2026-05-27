e57f730736854772672277555438f022b06893b1
Gabriel Martins Paz <th3gabrielmartins@gmail.com>
2026-05-26 22:47:52 -0300

    Remover importações não utilizadas em Product e User

--- src/main/java/com/example/ecommerce/demo/model/Product.java
+++ src/main/java/com/example/ecommerce/demo/model/Product.java
@@ -15,7 +15,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -50,6 +49,7 @@ public class Product {
     private String imageUrl;
 
     @Column(nullable = false)
+    @Default
     private Boolean active = true;
 
     @CreationTimestamp
