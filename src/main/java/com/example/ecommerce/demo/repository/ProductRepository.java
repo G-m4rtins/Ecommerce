078fcf21df056df6851b9e403eb9091c8423c66d
Gabriel Martins Paz <th3gabrielmartins@gmail.com>
2026-05-26 22:46:36 -0300

    Adicionar repositórios OrderRepository, ProductRepository e UserRepository com configuração básica.

---
+++ src/main/java/com/example/ecommerce/demo/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.example.ecommerce.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.ecommerce.demo.model.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
