d9a5fe33ab05608f6c01d51e86363e45279bc4b3
Gabriel Martins Paz <th3gabrielmartins@gmail.com>
2026-05-26 21:51:09 -0300

    Adicionar entidade Usuário com campos para id, nome, e-mail, senha, função e data/hora de criação; introduzir enumeração de Função

--- src/main/java/com/example/ecommerce/demo/model/User.java
+++ src/main/java/com/example/ecommerce/demo/model/User.java
@@ -1,5 +1,48 @@
 package com.example.ecommerce.demo.model;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.example.ecommerce.demo.model.enums.Role;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Email
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String password;
+
+    private Role role;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
---
+++ src/main/java/com/example/ecommerce/demo/model/enums/Role.java
@@ -0,0 +1,6 @@
+package com.example.ecommerce.demo.model.enums;
+
+public enum Role {
+    ADMIN,
+    CUSTOMER
+}
